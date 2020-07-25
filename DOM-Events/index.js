3c7b18add0c180f44ea4b0536f9c021484aa9383
Liana <bagiyanliana2@gmail.com>
2020-07-25 18:16:18 +0400

    added form-block and list-block

--- DOM-Events/index.js
+++ DOM-Events/index.js
@@ -1,50 +1,118 @@
 /**
  * Images clicks
-*/
+ */
 
 let photos = new Array(
     "./assets/emoji/lian-hi.PNG",
-    "./assets/emoji/lian-cool.PNG", 
-    "./assets/emoji/lian-smile.PNG",
+    "./assets/emoji/lian-cool.PNG",
     "./assets/emoji/lian-hmm.PNG",
-    "./assets/emoji/lian-give-an-eye.PNG",
     "./assets/emoji/lian-query.PNG",
-    "./assets/emoji/lian-yes.PNG",
-    "./assets/emoji/lian-finish.PNG",
-    "./assets/emoji/lian-happy-face.PNG",
-    "./assets/emoji/lian-mask.PNG"
-    )
+    "./assets/emoji/lian-smile.PNG",
+    "./assets/emoji/lian-yes.PNG"
+);
 
+let idName = new Array(
+    "block-2"
+)
+  
 let i = 0;
 let j = photos.length;
 let image = document.getElementById("image");
-
+  
 function arrowForward() {
-    if(i != j-1){
-        i++
+    if (i >= 0) {
+      document.getElementById("arrow-left").style.overflow = "visible";
     }
-    image.src = photos[i]
+  
+    if (i === j - 2) {
+      document.getElementById("arrow-right").style.overflow = "hidden";
+    }
+  
+    if (i != j - 1) {
+      i++;
+    }
+    image.src = photos[i];
 }
-
+  
 function arrowBack() {
-    if(i != 0){
-        i--
+    if (i === 1) {
+      document.getElementById("arrow-left").style.overflow = "hidden";
     }
-    image.src = photos[i]
+  
+    if (i === j - 1) {
+      document.getElementById("arrow-right").style.overflow = "visible";
+    }
+    if (i != 0) {
+      i--;
+    }
+    image.src = photos[i];
 }
+  
+/**
+    * Button click for DateTime
+*/
+  
+  let dateTimeText = document.getElementById("answer-text");
+  document.getElementById("btn").addEventListener("click", displayDate);
+  
+  function displayDate() {
+      if(dateTimeText.style.display !== "block"){
+          dateTimeText.style.display = "block"
+          dateTimeText.innerText = Date();
+      }else{
+          dateTimeText.style.display = "none"
+      }
+  }
+  
+/**
+   * Form --- input
+*/
+  
+const inputText = document.querySelector(".input");
+const inputBody = document.querySelector(".input-body");
+const submitBtn = document.querySelector(".button-form");
+  
+const state = {
+    inputValue: "",
+    todos: [],
+};
+  
+inputText.addEventListener("input", (event) => {
+    state.inputValue = event.target.value;
+});
+  
+submitBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    state.todos.push(state.inputValue);
+    state.inputValue = "";
+    inputText.value = "";
+  
+    render(state);
+});
+  
+const render = (state) => {
+    inputText.value = state.inputValue;
+  
+    const list = document.createElement("ol");
+  
+    state.todos.forEach((value) => {
+      const listItem = document.createElement("li");
+      listItem.innerText = value;
+      list.append(listItem);
+    });
+  
+    inputBody.innerText = "";
+    inputBody.append(list);
+};
+  
 
 /**
- * Button click for DateTime
+   * Schedule information list
 */
 
-let dateTimeText = document.getElementById("answer-text");
+const listItem = document.querySelectorAll(".item");
+const listText = document.querySelector(".item-text");
 
-function displayDate() {
-    if(dateTimeText.style.display !== "block"){
-        dateTimeText.style.display = "block"
-        dateTimeText.innerText = Date();
-    }else{
-        dateTimeText.style.display = "none"
-    }
-}
-
+listText.addEventListener('click', () => {
+    listText.style.textDecoration = "underline";
+})
