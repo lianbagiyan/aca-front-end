54a96f5fb011371dd55df90b8bbdf6e0ae7d6308
Liana <bagiyanliana2@gmail.com>
2020-07-28 01:28:59 +0400

    fixed dateTime task

--- DOM/dateTime/index.js
+++ DOM/dateTime/index.js
@@ -1,11 +1,11 @@
 let dateTimeText = document.getElementById("answer-text");
-document.getElementById("btn").addEventListener("click", displayDate);
-
-function displayDate() {
+document.getElementById("btn").addEventListener("click", function displayDate() {
   if (dateTimeText.style.display !== "block") {
     dateTimeText.style.display = "block";
     dateTimeText.innerText = Date();
   } else {
     dateTimeText.style.display = "none";
   }
-}
+});
+
+
