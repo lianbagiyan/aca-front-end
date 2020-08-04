b7630e8546fbe9b9e33041c110683cd791faea67
Liana <bagiyanliana2@gmail.com>
2020-08-04 16:43:43 +0400

    created card for films and added styles

--- API-fetch/index.js
+++ API-fetch/index.js
@@ -1,3 +1,15 @@
-fetch("https://restcountries.eu/rest/v2/all")
+let title = document.querySelector('.title-name');
+let date = document.querySelector('.date-number');
+let director = document.querySelector('.director-name');
+let description = document.querySelector('.description-text');
+
+fetch("https://ghibliapi.herokuapp.com/films")
     .then((r) => r.json())
-    .then(console.log);
+    .then((r) => {
+        console.log(r[0])
+        title.innerText = r[0].title;
+        date.innerText = r[0].release_date;
+        director.innerText = r[0].director;
+        description.innerText = r[0].description;
+    })
+    .then(console.log(title))
