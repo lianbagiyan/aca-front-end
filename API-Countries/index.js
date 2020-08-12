9da0361f81b132fb958f38945628fde3a7bd79f8
Liana <bagiyanliana2@gmail.com>
2020-08-13 01:57:37 +0400

    added js code for validation form, changed box-shadow color for btn and form-container

--- API-Countries/index.js
+++ API-Countries/index.js
@@ -1,63 +1,63 @@
-const form = document.getElementById('form');
-const login = document.getElementById('login');
-const password = document.getElementById('password');
-const email = document.getElementById('email');
+const form = document.getElementById("form");
+const login = document.getElementById("login");
+const password = document.getElementById("password");
+const email = document.getElementById("email");
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-    checkInputs();
-})
+  checkInputs();
+});
 
 function checkInputs() {
-    // get the values from the inputs
-    let loginValue = login.value.trim();
-    let emailValue = email.value.trim();
-    let passValue = password.value.trim();
-    
-    if(loginValue === '') {
-        setError(login, "Login cannot be blank.");
-    } else if(loginValue.length < 5) {
-        setError(login, "Login cannot be less than 5 characters.");
-    } else {
-        setSuccess(login);
-    }
+  // get the values from the inputs
+  let loginValue = login.value.trim();
+  let emailValue = email.value.trim();
+  let passValue = password.value.trim();
 
-    if(emailValue === '') {
-        setError(email, "Email cannot be blank.");
-    } else if(!isEmail(emailValue)) {
-        setError(email, "Email is not valid.");
-    } else {
-        setSuccess(email);
-    }
+  if (loginValue === "") {
+    setError(login, "Login cannot be blank.");
+  } else if (loginValue.length < 5) {
+    setError(login, "Login cannot be less than 5 characters.");
+  } else {
+    setSuccess(login);
+  }
 
-    if(passValue === '') {
-        setError(password, "Password cannot be blank.");
-    } else if(passValue.length <5) {
-        setError(password, "Password cannot be less than 5 characters.");
-    } else {
-        setSuccess(password);
-    }
+  if (emailValue === "") {
+    setError(email, "Email cannot be blank.");
+  } else if (!isEmail(emailValue)) {
+    setError(email, "Email is not valid.");
+  } else {
+    setSuccess(email);
+  }
+
+  if (passValue === "") {
+    setError(password, "Password cannot be blank.");
+  } else if (passValue.length < 5) {
+    setError(password, "Password cannot be less than 5 characters.");
+  } else {
+    setSuccess(password);
+  }
 }
 
 function setError(input, message) {
-    const formControl = input.parentElement;
-    const small = formControl.querySelector('small');
+  const formControl = input.parentElement;
+  const small = formControl.querySelector("small");
 
-    //add error message inside small
-    small.innerText = message;
+  //add error message inside small
+  small.innerText = message;
 
-    //add error class
-    formControl.className = 'form-control error'
+  //add error class
+  formControl.className = "form-control error";
 }
 
 function setSuccess(input) {
-    const formControl = input.parentElement;
+  const formControl = input.parentElement;
 
-    //add error class
-    formControl.className = 'form-control success'
+  //add error class
+  formControl.className = "form-control success";
 }
 
 function isEmail(email) {
-    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+  return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
 }
