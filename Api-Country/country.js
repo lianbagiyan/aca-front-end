7c299d3b3a1a034d38c16f05d7ec255f6fbadafd
Liana <bagiyanliana2@gmail.com>
2020-08-15 18:18:13 +0400

    created helper.js file

--- Api-Country/country.js
+++ Api-Country/country.js
@@ -1,6 +1,19 @@
+import { createCoutryTableRow } from './dom.helper.js';
+
+const state = document.querySelector('.state');
+const table = document.querySelector('.country-table');
+const tableBody = table.querySelector('tbody');
+
 fetch('https://restcountries.eu/rest/v2/all')
     .then((r) => r.json())
-    .then(console.log)
+    .then( (r) => {
+        r.forEach(({ name, flag }) => {
+            const row = createCoutryTableRow({name, imgSrc: flag});
+            tableBody.append(row);
+        })
+    })
     .catch( (err) => {
         console.warn(err);
+        state.innerText = err.message;
     })
+
